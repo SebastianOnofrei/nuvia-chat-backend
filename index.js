2e091d51053adff57cdc82ae65f967c5a7788246
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2025-04-09 02:06:34 +0300

    feat: installed SocketIO , created small socketIO server

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 // Use CORS middleware to allow all origins :)
 // #TODO: When deploying frontend to a dedicated URL, add just that URL in the allowed list
 app.use(cors());
 
-app.listen(4000, () => {
+// SocketIO server..
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.emit("welcome", "Hello, welcome to the server!");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(4000, () => {
   console.log("Server running on port 4000");
 });
