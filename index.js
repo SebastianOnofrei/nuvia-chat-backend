ef5b5b780b9b8b9dbca4792b363b5fb1690df31b
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2025-04-09 02:49:51 +0300

    feat: created small basic socket io server

--- index.js
+++ index.js
@@ -5,24 +5,72 @@ const { Server } = require("socket.io");
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
 
 // Use CORS middleware to allow all origins :)
 // #TODO: When deploying frontend to a dedicated URL, add just that URL in the allowed list
+
+// Store connected users' socket IDs
+let users = {}; // { userId: socketId }
+
 app.use(cors());
 
 // SocketIO server..
+let connectedUsers = 0;
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  // Increment the counter when a user connects
+  connectedUsers++;
+  console.log(`A user connected. Total connected users: ${connectedUsers}`);
 
   socket.emit("welcome", "Hello, welcome to the server!");
 
+  // When a user sends their ID (e.g., username), store the socket ID
+  socket.on("register", (userId) => {
+    users[userId] = socket.id;
+    console.log(`User ${userId} registered with socket ID: ${socket.id}`);
+  });
+
+  // Handle private message
+  socket.on("private_message", (data) => {
+    const { senderId, recipientId, message } = data;
+
+    // Send the message to the recipient if they are online (socket ID is stored)
+    const recipientSocketId = users[recipientId];
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit("private_message", {
+        senderId,
+        message,
+      });
+      console.log(
+        `Private message from ${senderId} to ${recipientId}: ${message}`
+      );
+    } else {
+      console.log(`User ${recipientId} is not connected.`);
+    }
+  });
+
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    connectedUsers--;
+    console.log(
+      `A user disconnected. Total connected users: ${connectedUsers}`
+    );
+
+    // Remove the user from the list of connected users
+    for (let userId in users) {
+      if (users[userId] === socket.id) {
+        delete users[userId];
+        console.log(`User ${userId} disconnected`);
+        break;
+      }
+    }
   });
 });
 
-server.listen(4000, () => {
-  console.log("Server running on port 4000");
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
